f0333ce254204f3e58d831f861aabb86bf8e67ee
tomguillermou <t.guillermou@citron.io>
2021-09-24 09:58:48 +0200

    ✅ Improve tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,32 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { User } from './user.schema';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
-  let service: UsersService;
+    let service: UsersService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
+    beforeEach(async () => {
+        const userModelMock = function (dto: any) {
+            this.data = dto;
+            this.save = () => this.data;
+        };
 
-    service = module.get<UsersService>(UsersService);
-  });
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                UsersService,
+                {
+                    provide: getModelToken(User.name),
+                    useValue: userModelMock,
+                },
+            ],
+        }).compile();
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+        service = module.get<UsersService>(UsersService);
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
