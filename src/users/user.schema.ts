57b304ce0aac3b9a5dd11f5b0c7d93efbc33a78a
tomguillermou <tomguillermou@gmail.com>
2022-06-24 21:03:21 +0200

    🐛 Fix strictness from user schema

--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -4,10 +4,10 @@ import { Document, Types } from 'mongoose';
 @Schema()
 export class User {
     @Prop({ required: true })
-    email: string;
+    email!: string;
 
     @Prop({ required: true, select: false })
-    password: string;
+    password!: string;
 }
 
 export type UserDocument = User & Document<Types.ObjectId>;
